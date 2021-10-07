c53f7917dc1dc6132863e2d8a1f48bf52b8f9045
Julian Stobbe <Atomtomate@gmx.de>
2021-10-07 13:56:49 +0200

    activated omega tail correction

--- new_lambda_analysis.jl
+++ new_lambda_analysis.jl
@@ -63,7 +63,7 @@ function c2_along_λsp_of_λch(λch_range::AbstractArray{Float64,1}, spOfch::Abs
     corr = Σ_correction(ωindices, bubble, FUpDo, sP)
     (sP.tc_type_f != :nothing) && LadderDGA.extend_corr!(corr)
 
-    νmax = trunc(Int,size(bubble,LadderDGA.ν_axis)/3)
+    νmax = floor(Int,length(ωindices)/3)
     νGrid = 0:(νmax-1)
     iν_n = LadderDGA.iν_array(mP.β, νGrid)
     Σ_hartree = mP.n * mP.U/2
@@ -95,27 +95,24 @@ function c2_along_λsp_of_λch(λch_range::AbstractArray{Float64,1}, spOfch::Abs
         #TODO: sc here, this could be inclines to avoid realloc of Σ_ladder_ω
 
         LadderDGA.calc_Σ_ω!(Σ_λ_ω, Kνωq, Kνωq_pre, ωindices, χsp, nlQ_sp.γ, χch, nlQ_ch.γ, GLoc_fft, corr, mP.U, kG, sP)
-        @inbounds @views Σ_λ[:,:] = (mP.U/mP.β) .* sum(Σ_λ_ω, dims=[3])[:,:,1] .+ Σ_hartree
-        Σ_λ = Σ_loc_correction(Σ_λ, Σ_ladderLoc, Σ_loc)
+        @inbounds @views Σ_λ[:,:] = (mP.U/mP.β) .* sum(Σ_λ_ω, dims=[3])[:,:,1]
+        Σ_λ = Σ_loc_correction(Σ_λ, Σ_ladderLoc, Σ_loc) .+ Σ_hartree
 
-        #@simd for qi in 1:size(Σ_λ, 1)
-        #    @inbounds view(Σ_λ, qi, :) = view(Σ_λ, qi, :) .+ Σ_corr
-        #end
         for (j,ek) in enumerate(kG.ϵkGrid)
             for ni in 0:νmax-1
                 @inbounds G_λ[j,ni+1] = LadderDGA.G_from_Σ(ni, mP.β, mP.μ, ek, Σ_λ[j,ni+1])
             end
         end
-        println("aaa ", Σ_λ[4,2:8])
         G_λ2 = transpose(LadderDGA.flatten_2D(LadderDGA.G_from_Σ(Σ_λ, kG.ϵkGrid, νGrid, mP)));
         #TODO: sc end here
         EPot = LadderDGA.calc_E_pot(kG, G_λ, Σ_λ, E_pot_tail, E_pot_tail_inv, mP.β)
+
         for (wi,w) in enumerate(ωindices)
             @inbounds χupup_ω[wi] = real(kintegrate(kG, view(χch,:,w) .+ view(χsp,:,w))[1]) / 2
             @inbounds χupdo_ω[wi] = real(kintegrate(kG, view(χch,:,w) .- view(χsp,:,w))[1]) / 2
         end
-        #χupup_ω = subtract_tail(χupup_ω, mP.Ekin_DMFT, iωn)
-        lhs_c1 = sum(χupup_ω)/mP.β #- mP.Ekin_DMFT*mP.β/12
+        χupup_ω = subtract_tail(χupup_ω, mP.Ekin_DMFT, iωn)
+        lhs_c1 = sum(χupup_ω)/mP.β - mP.Ekin_DMFT*mP.β/12
         lhs_c2 = sum(χupdo_ω)/mP.β
         rhs_c1 = mP.n/2 * (1 - mP.n/2)
         rhs_c2 = EPot/mP.U - (mP.n/2) * (mP.n/2)
